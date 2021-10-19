0d7dc5a13b6d289f269f1f689a15402b7664fdeb
Lucas Silva <martssilvapro@gmail.com>
2021-10-19 11:11:02 -0300

    implementa limpeza de dados e istala pymongo

--- teste.py
+++ teste.py
@@ -1,19 +1,16 @@
 from parsel import Selector
 import requests
 
-URL_BASE = "http://books.toscrape.com/catalogue/"
 
-response = requests.get(URL_BASE + "page-1.html")
+response = requests.get("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
 selector = Selector(text=response.text)
 
-href = selector.css(".product_pod h3 a::attr(href)").get()
-detail_page_url = URL_BASE + href
+# Extrai a descrição
+description = selector.css("#product_description ~ p::text").get()
+print(description)
 
-# baixa o conteúdo da página de detalhes
-detail_response = requests.get(detail_page_url)
-detail_selector = Selector(text=detail_response.text)
-
-# recupera a descrição do produto
-# ~ significa a tag irmã
-description = detail_selector.css("#product_description ~ p::text").get()
+# "Fatiamos" a descrição removendo o sufixo
+suffix = "...more"
+if description.endswith(suffix):
+    description = description[:-len(suffix)]
 print(description)
